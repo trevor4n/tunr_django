ca7ffa95fb8eac7d66ab8971a270619ee28317d5
trevor4n <trevor4n@gmail.com>
2021-08-14 12:13:10 -0500

    Django Models

--- tunr/models.py
+++ tunr/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.name
