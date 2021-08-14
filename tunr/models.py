8376d286cce4fc3170fe1ca18605970fbf805395
trevor4n <trevor4n@gmail.com>
2021-08-14 14:42:22 -0500

    migrations

--- tunr/models.py
+++ tunr/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Artist(models.Model):
+class Artist(models.Model): # defining 3 fields ~ columns
     name = models.CharField(max_length=100)
     nationality = models.CharField(max_length=100)
     photo_url = models.TextField()
 
-    def __str__(self):
-        return self.name
+    def __str__(self): # double underscore ~ python dunder
+        return self.name # This method defines what an instance of the model will show up as by default.
