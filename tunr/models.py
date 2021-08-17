af4c670fb592ec6642a7575772531987a5178a74
trevor4n <trevor4n@gmail.com>
2021-08-17 17:47:04 -0500

    Song model w/ artist foreign key

--- tunr/models.py
+++ tunr/models.py
@@ -4,7 +4,14 @@ from django.db import models
 class Artist(models.Model): # defining 3 fields ~ columns
     name = models.CharField(max_length=100)
     nationality = models.CharField(max_length=100)
-    photo_url = models.TextField()
+    photo_url = models.TextField(default='blank')
 
     def __str__(self): # double underscore ~ python dunder
         return self.name # This method defines what an instance of the model will show up as by default.
+
+class Song(models.Model):
+    artist = models.ForeignKey(
+        Artist, 
+        on_delete=models.CASCADE, 
+        related_name='songs'
+    )
