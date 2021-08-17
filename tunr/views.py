0204c32c734caba1346355a8d00bf9bb6070e91e
trevor4n <trevor4n@gmail.com>
2021-08-17 18:43:50 -0500

    view fx

--- tunr/views.py
+++ tunr/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Artist, Song
 # Create your views here.
+def artist_list(req):
+    artists = Artist.objects.all()  # artists QuerySet
+    return render(req, 'tunr/artist_list.html', {'artists': artists}) # request argument, render template, dictionary
+
+def song_list(req):
+    songs = Song.objects.all()
+    return render(req, 'tunr/song_list.html', {'songs': songs})
